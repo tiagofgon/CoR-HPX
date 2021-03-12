ee719da7ad8ccd10305350b20d6ed9c455283c1a
tiagofgon <tiago.fg@outlook.com>
2021-03-12 17:43:39 +0000

    mudança de nomes

---
+++ libcor/include/cor/CoR-HPXConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for PlaCoR
+#define CoRHPX_VERSION_MAJOR 1
+#define CoRHPX_VERSION_MINOR 0
--- libcor/include/cor/PlaCoRConfig.h
+++
@@ -1,3 +0,0 @@
-// the configured options and settings for PlaCoR
-#define PlaCoR_VERSION_MAJOR 1
-#define PlaCoR_VERSION_MINOR 0
